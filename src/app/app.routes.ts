7ff4887e1d3fba12a8874d70d0ec5a38374081e9
Rahul KR <rahulramakrishnan512@gmail.com>
2025-12-06 13:37:48 +0530

    application structure and basic login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
